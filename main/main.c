93db58598ac4996fe8255b329e503cf22be6138b
hiruna <hiruna.kawinda@gmail.com>
2023-05-27 11:56:43 +1000

    add esp_timer and fix lvgl drv calls

--- main/main.c
+++ main/main.c
@@ -19,6 +19,7 @@
 #include "freertos/semphr.h"
 #include "esp_system.h"
 #include "driver/gpio.h"
+#include "esp_timer.h"
 
 /* Littlevgl specific */
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
@@ -93,7 +94,7 @@ static void guiTask(void *pvParameter) {
     static lv_color_t *buf2 = NULL;
 #endif
 
-    static lv_disp_buf_t disp_buf;
+    static lv_disp_draw_buf_t disp_buf;
 
     uint32_t size_in_px = DISP_BUF_SIZE;
 
@@ -108,7 +109,7 @@ static void guiTask(void *pvParameter) {
 
     /* Initialize the working buffer depending on the selected display.
      * NOTE: buf2 == NULL when using monochrome displays. */
-    lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
+    lv_disp_draw_buf_init(&disp_buf, buf1, buf2, size_in_px);
 
     lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
@@ -126,7 +127,7 @@ static void guiTask(void *pvParameter) {
     disp_drv.set_px_cb = disp_driver_set_px;
 #endif
 
-    disp_drv.buffer = &disp_buf;
+    disp_drv.draw_buf = &disp_buf;
     lv_disp_drv_register(&disp_drv);
 
     /* Register an input device when enabled on the menuconfig */
